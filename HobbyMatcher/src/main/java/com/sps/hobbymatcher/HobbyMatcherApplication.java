d21f5e70257c47494d0735bbc6fe9757058ed4c7
Maliha <malihabakhshi01@gmail.com>
2020-07-28 20:51:56 +0530

    initial check-in of the project

---
+++ HobbyMatcher/src/main/java/com/sps/hobbymatcher/HobbyMatcherApplication.java
@@ -0,0 +1,13 @@
+package com.sps.hobbymatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbyMatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbyMatcherApplication.class, args);
+	}
+
+}
---
+++ HobbyMatcher/src/test/java/com/sps/hobbymatcher/HobbyMatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sps.hobbymatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyMatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
