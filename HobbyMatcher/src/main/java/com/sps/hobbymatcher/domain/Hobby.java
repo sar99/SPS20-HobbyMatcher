fda05f3fba8247ca33ab8e3f1c1dafaa62d7d111
Maliha <malihabakhshi01@gmail.com>
2020-08-04 20:08:12 +0000

    create database

---
+++ HobbyMatcher/src/main/java/com/sps/hobbymatcher/domain/Hobby.java
@@ -0,0 +1,54 @@
+package com.sps.hobbymatcher.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.cloud.gcp.data.datastore.core.mapping.Entity;
+
+@Entity(name = "hobbies")
+public class Hobby {
+	
+	@Id
+	private Long id;
+	private String name;
+	private Set<User> users=new HashSet<>();
+	private Set<Post> posts=new HashSet<>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(Set<User> users) {
+		this.users = users;
+	}
+
+	public Set<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(Set<Post> posts) {
+		this.posts = posts;
+	}
+
+    @Override
+	public String toString() {
+		return "Hobby [id=" + id + ", name=" + name + ", users=" + users + ", posts=" + posts + "]";
+	}
+}
---
+++ HobbyMatcher/src/main/java/com/sps/hobbymatcher/domain/User.java
@@ -0,0 +1,69 @@
+package com.sps.hobbymatcher.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.cloud.gcp.data.datastore.core.mapping.Entity;
+
+@Entity(name="users")
+public class User {
+	
+	@Id
+	private Long id;
+	private String name;
+	private String username;
+	private String password; 
+	private Set<User> connections=new HashSet<>();
+	private Set<Hobby> myHobbies=new HashSet<>();
+	private Set<Post> myPosts=new HashSet<>();
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Set<User> getConnections() {
+		return connections;
+	}
+	public void setConnections(Set<User> connections) {
+		this.connections = connections;
+	}
+	public Set<Hobby> getMyHobbies() {
+		return myHobbies;
+	}
+	public void setMyHobbies(Set<Hobby> myHobbies) {
+		this.myHobbies = myHobbies;
+	}
+	public Set<Post> getMyPosts() {
+		return myPosts;
+	}
+	public void setMyPosts(Set<Post> myPosts) {
+		this.myPosts = myPosts;
+	}
+    
+    @Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", username=" + username + ", password=" + password + ", connections=" + connections + ", myHobbies=" + myHobbies + ", myPosts="
+				+ myPosts + "]";
+	}
+}
