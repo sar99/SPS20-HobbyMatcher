454c1516122106b15ade1e7db0d0339e1137af80
Maliha <malihabakhshi01@gmail.com>
2020-08-13 09:11:17 +0000

    push complete project

---
+++ HobbyMatcher/src/test/java/com/sps/hobbymatcher/HobbyMatcherApplicationTests.java
@@ -0,0 +1,27 @@
+package com.sps.hobbymatcher;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.*;
+
+import com.sps.hobbymatcher.domain.User;
+import com.sps.hobbymatcher.domain.Post;
+import com.sps.hobbymatcher.domain.Hobby;
+import com.sps.hobbymatcher.service.UserService;
+import com.sps.hobbymatcher.service.PostService;
+import com.sps.hobbymatcher.service.HobbyService;
+import com.sps.hobbymatcher.repository.HobbyRepository;
+import com.sps.hobbymatcher.repository.UserRepository;
+import com.sps.hobbymatcher.repository.PostRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Ignore
+public class HobbyMatcherApplicationTests {
+
+}
