0d4f94bde9458275b67aca5f1bca629c58d2e471
Maliha <malihabakhshi01@gmail.com>
2020-08-16 09:02:41 +0000

    modify login and register page

--- HobbyMatcher/src/main/java/com/sps/hobbymatcher/controller/LoginController.java
+++ HobbyMatcher/src/main/java/com/sps/hobbymatcher/controller/LoginController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 import com.sps.hobbymatcher.domain.User;
 import com.sps.hobbymatcher.service.UserService;
@@ -21,18 +22,22 @@ public class LoginController {
     private UserRepository userRepository;
     
     @GetMapping("/login")
-    public String login() {
-        
+    public String login(@AuthenticationPrincipal User user) {
+        if(user != null) {
+            return "dashboard";
+        }
         return "login";
     }
     
     @GetMapping("/register")
-    public String register (ModelMap model) {
+    public String register (@AuthenticationPrincipal User user, ModelMap model) {
 
+        if(user != null) {
+            return "dashboard";
+        }
         model.put("user", new User());
 
         return "register";
-
     }
     
     @PostMapping("/register")
@@ -40,6 +45,6 @@ public class LoginController {
 
         userService.registerUser(user);
         
-        return "redirect:/login";
+        return "redirect:/dashboard";
     }
 }
