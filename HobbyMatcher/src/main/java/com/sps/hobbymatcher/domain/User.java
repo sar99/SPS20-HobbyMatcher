f4315415ef56b3b06d6f44fe141357c9a819cffc
Saniya Arora <saniya.rishi@gmail.com>
2020-08-23 10:01:05 +0000

    add bio to dashboard

--- HobbyMatcher/src/main/java/com/sps/hobbymatcher/domain/User.java
+++ HobbyMatcher/src/main/java/com/sps/hobbymatcher/domain/User.java
@@ -2,6 +2,7 @@ package com.sps.hobbymatcher.domain;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.io.*; 
 
 import org.springframework.data.annotation.Id;
 import org.springframework.cloud.gcp.data.datastore.core.mapping.Entity;
@@ -20,6 +21,8 @@ public class User implements UserDetails {
 	private Set<String> connections=new HashSet<>();
 	private Set<Long> myHobbies=new HashSet<>();
     private Set<Authority> authorities = new HashSet<>();
+    private String bio;
+    private File profilePic;
 
 	@Override
 	public boolean isAccountNonExpired() {
@@ -83,11 +86,23 @@ public class User implements UserDetails {
 	public void setAuthorities (Set<Authority> authorities) {
 		this.authorities = authorities;
 	}
+    public String getBio() {
+		return bio;
+	}
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
+    public File getProfilePic() {
+		return profilePic;
+	}
+	public void setProfilePic(File profilePic) {
+		this.profilePic = profilePic;
+	}
     
     @Override
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", username=" + username + 
-                      ", password=" + password + ", connections=" + connections + 
-                      ", myHobbies=" + myHobbies +", authorities=" + authorities+"]";
+                       ", password=" + password + ", bio=" + bio + ", profilePic=" + profilePic + 
+                      ", connections=" + connections + ", myHobbies=" + myHobbies +", authorities=" + authorities+"]";
 	}
 }
