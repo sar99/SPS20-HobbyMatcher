757c40124070831791952b477755b230d165cff4
Saniya Arora <saniya.rishi@gmail.com>
2020-08-25 03:36:49 +0000

    handle null hobbies situation for random selector

--- HobbyMatcher/src/main/resources/static/js/index.js
+++ HobbyMatcher/src/main/resources/static/js/index.js
@@ -1,54 +1,35 @@
-function sleep(milliseconds) {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
-}
-
-
 function randomSelector()
 {
-    // console.log(hobbies);
-    // var randomCard = document.getElementById("random-hobby");
-
-    // randomCard.style.display = "block";
-
-    // var i =0, k = 20;
-    // while(k--)
-    // {
-
-    //     i = Math.floor((Math.random() * hobbies.length));
-    //     console.log(i);
-    //     // randomCard.childNodes[0] = 
-    //     var url = "/hobbies/" + hobbies[i].id;
-    //     document.getElementById("random-title").innerHTML = hobbies[i].name; 
-    //     document.getElementById("random-url").setAttribute("href", url);
-    //     sleep(750);
-
-    // }
-
-    console.log("before");
-    var randomCard = document.getElementById("random-hobby");
-    randomCard.style.display = "block";
-
-    console.log("after");
-    var k = 0, i;
-
-    function counter() {
-        k++;
-        i = Math.floor((Math.random() * hobbies.length));
-        console.log(i);
-        // randomCard.childNodes[0] = 
-        var url = "/hobbies/" + hobbies[i].id;
-        document.getElementById("random-title").innerHTML = hobbies[i].name; 
-        document.getElementById("random-url").setAttribute("href", url);
-        if (k <= 20) {
-            setTimeout(counter, 150);
-        }
+    if(hobbies.length === 0)
+    {
+        document.getElementById("random-hobby").style.display="none";
+        document.getElementById("no-random-hobby").style.display="block";
     }
+    else
+    {
+        console.log("before");
+        document.getElementById("no-random-hobby").style.display="none";
+        var randomCard = document.getElementById("random-hobby");
+        randomCard.style.display = "block";
 
-    counter();
+        console.log("after");
+        var k = 0, i;
+
+        function counter() {
+            k++;
+            i = Math.floor((Math.random() * hobbies.length));
+            console.log(i);
+            // randomCard.childNodes[0] = 
+            var url = "/hobbies/" + hobbies[i].id;
+            document.getElementById("random-title").innerHTML = hobbies[i].name; 
+            document.getElementById("random-url").setAttribute("href", url);
+            if (k <= 20) {
+                setTimeout(counter, 150);
+            }
+        }
+
+        counter();
+    }
 
 
 }
